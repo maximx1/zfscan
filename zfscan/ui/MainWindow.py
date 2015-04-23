8dc009dba11740baa2a18f6e45708373fcc2a063
Justin Walrath <walrathjaw@gmail.com>
2015-04-22 21:52:11 -0500

    Added button

--- zfscan/ui/MainWindow.py
+++ zfscan/ui/MainWindow.py
@@ -5,10 +5,21 @@ from PySide.QtGui import *
 class MainWindow:
     def __init__(self, width, height):
         app = QApplication([])
-        mainWidget = QWidget()
-        layout = QVBoxLayout(mainWidget)
-        label = QLabel("<font color=red size=40>Hello, World!</font>")
-        layout.addWidget(label)
-        mainWidget.resize(width, height)
-        mainWidget.show()
+        main_window = QWidget()
+
+        self.label = QLabel("<font color=red size=40>Push Button for something fun!</font>")
+        self.button1 = QPushButton("Push Me!")
+
+        main_window.setLayout(self.build_layout([self.label, self.button1]))
+        main_window.resize(width, height)
+        main_window.show()
         sys.exit(app.exec_())
+
+    def build_layout(self, widgets):
+        layout = QVBoxLayout()
+        for i in widgets:
+            layout.addWidget(i)
+        return layout
+
+    def update_label(self):
+        """"""
